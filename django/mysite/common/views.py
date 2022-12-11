eafaff139ed300816ec723c4656a062d822624f8
ikefreet <117957214+ikefreet@users.noreply.github.com>
2022-12-11 17:30:24 +0900

    Update views.py

--- django/mysite/common/views.py
+++ django/mysite/common/views.py
@@ -12,8 +12,7 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            if not user:
-            else:
+            if user:
                 login(request, user)
             return redirect('index')
     else:
