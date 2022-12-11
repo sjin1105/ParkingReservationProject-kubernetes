5cb1ca863a53c9095d7fe9d4409143b933455f13
parlyresk <72953981+parlyresk@users.noreply.github.com>
2022-12-11 18:00:47 +0900

    Update views.py

--- django/mysite/common/views.py
+++ django/mysite/common/views.py
@@ -12,13 +12,12 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            if user:
-                login(request, user)
-                print(username)
-                print(raw_password)
-                print(user)
-            else:
-                print('error')
+       
+            login(request, user)
+            print(username)
+            print(raw_password)
+            print(user)
+                
             return redirect('index')
     else:
         form = UserForm()
