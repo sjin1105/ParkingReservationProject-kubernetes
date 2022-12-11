f7d6935d7fcdb2e71c32ab0b1c4ce53a203b2737
ikefreet <117957214+ikefreet@users.noreply.github.com>
2022-12-11 16:53:07 +0900

    Update views.py

--- django/mysite/common/views.py
+++ django/mysite/common/views.py
@@ -12,7 +12,7 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            login(request, user)  # 로그인
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')  # 로그인
             return redirect('index')
     else:
         form = UserForm()
